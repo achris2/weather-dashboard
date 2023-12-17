c96d78a646b724a38d58adeff20fb38274a00062
Alex Christakou <alex@Alexs-Air-2.broadband>
2023-12-17 23:04:10 +0000

    main card now working

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@
 $('#search-button').on('click', function(event){
     event.preventDefault();
     let chosenCity = $('#search-input').val();
-    let queryURL ="https://api.openweathermap.org/data/2.5/forecast?q=" + chosenCity + "&units=&appid=fa4695e0608a76d517ec72dbb80b9028&units=metric";
+    let queryURL ="https://api.openweathermap.org/data/2.5/forecast?q=" + chosenCity + "&units=&appid=fa4695e0608a76d517ec72dbb80b9028";
     console.log(queryURL);
 
     // documentation https://openweathermap.org/forecast5 
@@ -23,14 +23,17 @@ $('#search-button').on('click', function(event){
         // Date 
         // get timestamp in Unix, which is no of seconds since 1970
         // Need to convert convert unix Time Stamp to Date 
-        let currentTime = new Date(data.list[0].dt*10000);
-        
-      
+        let currentTime = new Date(data.list[0].dt * 1000);
 
-        let todayDiv = $('#today'); 
+        // format Date 
+        let formattedDate = ('0' + currentTime.getDate()).slice(-2) + ' /' + ('0' + (currentTime.getMonth() + 1)).slice(-2) + ' /' + currentTime.getFullYear();
+        
+    
+
+        let todayDiv = $('#today-div'); 
 
         //  Temperature 
-        let temperature = data.list[0].main.temp;
+        let tempCelsius = Math.round(data.list[0].main.temp-273.15); Math.round()
 
         // Wind Speed 
 
@@ -42,13 +45,19 @@ $('#search-button').on('click', function(event){
         let humidity = data.list[0].main.humidity;
         console.log(humidity);
 
-        // Icon 
-        let icon = data.list[0].weather.icon;
+        // Icon -         //Icon URL
+        // https://openweathermap.org/img/w/' + weather[0].icon + '.png
+        let icon = data.list[0].weather[0].icon;
+        let iconURL = 'https://openweathermap.org/img/w/' + icon + '.png'; 
         console.log(icon);
+        console.log(iconURL);
 
 
-        todayDiv.append(`<p>Date: ${currentTime}</p>`);
-        todayDiv.append(currentTime, temperature, windSpeed,humidity,icon);
+
+        todayDiv.append(`<h2>${chosenCity} (${formattedDate}) <img src="${iconURL}"> </h2>`);
+        todayDiv.append(`<p>Temp: ${tempCelsius} °C</p>`);
+        todayDiv.append(`<p>Wind Speed: ${windSpeed} KPH</p>`);
+        todayDiv.append(`<p>Humidity: ${humidity}%</p>`);
 
     }) 
 
