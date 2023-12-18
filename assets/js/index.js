bb2ce4db1fb94a6f6f594a47b743ae79f757eae5
Alex Christakou <alex@Alexs-Air-2.broadband>
2023-12-18 00:40:13 +0000

    added search history to the app

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
+// Load search history from localStorage on page load
+$(document).ready(function () {
+    loadSearchHistory();
+});
+
 // add event listener to form submit 
 
 $('#search-button').on('click', function(event){
@@ -18,7 +23,21 @@ $('#search-button').on('click', function(event){
 // //          - wind speed
 // //          - humidity
 // //          - icon
-        console.log(data);        
+        console.log(data);
+
+        // selectors of the history div, forecast div & main div
+        let todayDiv = $('#today-div'); 
+        let historyDiv = $('#history');
+        let forecastCards = $('#forecast-cards'); 
+
+        // empties main card & forecast cards 
+
+        todayDiv.empty();
+        forecastCards.empty();
+
+        // add to City to Search History Button
+
+        historyDiv.append(`<button type="button" class="btn btn-secondary"> ${chosenCity} </button>`) 
 
         // Date 
         // get timestamp in Unix, which is no of seconds since 1970
@@ -28,10 +47,6 @@ $('#search-button').on('click', function(event){
         // format Date 
         let formattedDate = ('0' + currentTime.getDate()).slice(-2) + ' /' + ('0' + (currentTime.getMonth() + 1)).slice(-2) + ' /' + currentTime.getFullYear();
         
-    
-
-        let todayDiv = $('#today-div'); 
-
         //  Temperature 
         let tempCelsius = Math.round(data.list[0].main.temp-273.15); Math.round()
 
@@ -52,16 +67,55 @@ $('#search-button').on('click', function(event){
         console.log(icon);
         console.log(iconURL);
 
-
-
+        // appends the info on chosen city to main card 
         todayDiv.append(`<h2>${chosenCity} (${formattedDate}) <img src="${iconURL}"> </h2>`);
         todayDiv.append(`<p>Temp: ${tempCelsius} °C</p>`);
         todayDiv.append(`<p>Wind Speed: ${windSpeed} KPH</p>`);
         todayDiv.append(`<p>Humidity: ${humidity}%</p>`);
 
+
+        // loops through the API result and generates the cards 
+
+        for (let i=0; i< data.list.length; i++){
+            let currentTime = new Date(data.list[i].dt * 1000);
+            let formattedDate = ('0' + currentTime.getDate()).slice(-2) + '/' + ('0' + (currentTime.getMonth() + 1)).slice(-2) + '/' + currentTime.getFullYear();
+            let tempCelsius = Math.round(data.list[i].main.temp-273.15); Math.round();
+            let windSpeed = data.list[i].wind.speed;
+            let humidity = data.list[i].main.humidity;
+            let icon = data.list[i].weather[0].icon;
+            let iconURL = 'https://openweathermap.org/img/w/' + icon + '.png';
+            let forecastCards = $('#forecast-cards'); 
+
+            forecastCards.append(`
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"> ${formattedDate} </h5>
+                        <p class="card-text">
+                            <p><img src="${iconURL}"></p>
+                            <p>Temp: ${tempCelsius} °C</p>
+                            <p>Wind Speed: ${windSpeed} KPH</p>
+                            <p>Humidity: ${humidity}%</p>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        `);
+
+
+        }
+
+
+        
+
     }) 
 
 })
+
+
+
+
+
 
 // get the user input value 
 
